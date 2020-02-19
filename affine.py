ccfba47cbe76df7a9120ddae26c58e2fa4d41b62
vandemjh <jack.vandem@gmail.com>
2020-02-19 12:51:29 -0500

    added new function

--- affine.py
+++ affine.py
@@ -5,14 +5,13 @@ if (len(sys.argv) != 6):
     exit()
 
 operation = sys.argv[1]
-file1 = sys.argv[2]
-file2 = sys.argv[3]
 a = int(sys.argv[4])
 b = int(sys.argv[5])
 
 #TODO this
-if (False):
+if (a % 2 == 0 or a < 1 or a > 127 or b < 1 or b > 127):
     print("The key pair (", a, ", ", b, ") is invalid, please select another key.")
+    exit();
 
 # Given a character of our plaintext message m,
 # the encryption function E(m, a, b) is given by:
@@ -40,6 +39,8 @@ def egcd (a , b):  #integers with a > b > 0
     return d, s, t # as+b t = d and gcd ( a , b ) = d
 
 def encrypt(input, a , b):
+    file1 = sys.argv[2]
+    file2 = sys.argv[3]
     toReturn = ""
     for char in input:
         # chr function changes value to ascii char
@@ -48,8 +49,13 @@ def encrypt(input, a , b):
     return toReturn
 
 def decrypt(input, a , b):
+    file1 = sys.argv[2]
+    file2 = sys.argv[3]
     toReturn = ""
     return toReturn
+
+def decypher(input, output, dictionary):
+    print dictionary
 
 #print("Starting...")
 
