c47f7796ee5cd3d164878649f06681ebf5c3996f
vandemjh <jack.vandem@gmail.com>
2020-02-19 02:34:47 -0500

    finished encrypt

--- affine.py
+++ affine.py
@@ -7,10 +7,36 @@ if (len(sys.argv) != 3):
 input = sys.argv[1]
 output = sys.argv[2]
 
-def encrypt(input, output):
-    r = open(str(input), "r")
-    w = open(str(output),"w")
-    w.write(r);
 
-print("Starting...")
-encrypt(input, output)
+# Given a character of our plaintext message m,
+# the encryption function E(m, a, b) is given by:
+# E(m, a, b) = (a · m + b) mod 128.
+def E(m, a, b):
+    return (a * m + b) % 128
+
+def encrypt(input, a , b):
+    toReturn = ""
+    for char in input:
+        # chr function changes value to ascii char
+        # ord changes char to int value
+        toReturn += chr(E(ord(char), a, b))
+    return toReturn
+
+def decrypt(input, output, a , b):
+    toReturn = ""
+    for char in input:
+        toReturn += chr(E(ord(char), a, b))
+    return toReturn
+
+#print("Starting...")
+
+reader = open(str(input), "r")
+writer = open(str(output),"w")
+
+input = reader.read()
+#writer = output.write()
+
+encrypted = encrypt(input, 1, 1)
+#decrypted = decrypt(output, input)
+
+print (encrypted)
