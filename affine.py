071d45cb5168643de3acd48f4fb11428cec24291
vandemjh <jack.vandem@gmail.com>
2020-02-19 22:00:49 -0500

    completed encrypt

--- affine.py
+++ affine.py
@@ -1,14 +1,7 @@
 import sys
+import math
 
-if (len(sys.argv) != 6):
-    print("Invalid number of arguments, 5 expected\n")
-    exit()
-
-operation = sys.argv[1]
-file1 = sys.argv[2]
-file2 = sys.argv[3]
-a = int(sys.argv[4])
-b = int(sys.argv[5])
+# --- Function Definition ---#
 
 # Given a character of our plaintext message m,
 # the encryption function E(m, a, b) is given by:
@@ -16,7 +9,10 @@ b = int(sys.argv[5])
 def E(m, a, b):
     return (a * m + b) % 128
 
-def D(a, m):
+def D(m, a, b):
+    return (inverseMod(a, 128) * (m - b)) % 128
+
+def inverseMod(a, m):
     a = a % m
     for x in range(1, m):
         if ((a * x) % m == 1):
@@ -36,9 +32,9 @@ def egcd (a , b):  #integers with a > b > 0
     return d, s, t # as+b t = d and gcd ( a , b ) = d
 
 def encrypt(input, a , b):
-    if (a % 2 == 0 or a < 1 or a > 127 or b < 1 or b > 127):
+    if (math.gcd(a,b) != 1 or a < 1 or a > 127 or b < 1 or b > 127):
         print("The key pair (", a, ", ", b, ") is invalid, please select another key.")
-        exit();
+        exit()
     toReturn = ""
     for char in input:
         # chr function changes value to ascii char
@@ -47,25 +43,48 @@ def encrypt(input, a , b):
     return toReturn
 
 def decrypt(input, a , b):
-    if (a % 2 == 0 or a < 1 or a > 127 or b < 1 or b > 127):
+    if (math.gcd(a,b) != 1 or a < 1 or a > 127 or b < 1 or b > 127):
         print("The key pair (", a, ", ", b, ") is invalid, please select another key.")
-        exit();
+        exit()
     toReturn = ""
+    for char in input:
+        toReturn += chr(D(ord(char), a, b))
     return toReturn
 
+
 def decypher(input, output, dictionary):
-    print dictionary
+    print (dictionary)
 
-#print("Starting...")
+# --- Command Line Parsing ---#
 
-reader = open(str(file1), "r")
-writer = open(str(file2),"w")
+if (len(sys.argv) != 6):
+    print("Invalid number of arguments, 5 expected\n")
+    exit()
 
-input = reader.read()
-#writer = output.write()
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+arg3 = sys.argv[3]
+arg4 = sys.argv[4]
+arg5 = sys.argv[5]
 
-encrypted = encrypt(input, a, b)
-#decrypted = decrypt(encrypted, 1, 1)
+if (str(arg1) == "encrypt"):
+    reader = open(str(arg2), "r")
+    writer = open(str(arg3),"w")
+    input = reader.read()
+    a = int(arg4)
+    b = int(arg5)
+    encrypted = encrypt(input, a, b)
+    writer.write(encrypted)
+    reader.close()
+    writer.close()
 
-print (encrypted)
-#print(decrypted)
+elif (str(arg1) == "decrypt"):
+    reader = open(str(arg2), "r")
+    writer = open(str(arg3),"w")
+    input = reader.read()
+    a = int(arg4)
+    b = int(arg5)
+    decrypted = decrypt(input, a, b)
+    writer.write(decrypted)
+    reader.close()
+    writer.close()
